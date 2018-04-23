0762f2440b3e0241e8741bba436dbe8981f70090
leyei <t7olif00@students.oamk.fi>
2018-04-24 00:39:42 +0300

    Updated outside pages with loans - lost initial version

--- oamk_bank/application/views/Page_Outside/loan.php
+++ oamk_bank/application/views/Page_Outside/loan.php
@@ -1,65 +1,28 @@
 
-    <div class="nav-bar">
-      <div id="nav-container">
-          <a href="http://www.oamkbank.gq">
-            <img class="logo" src="img/oamkb-logo-transp.png"/></a>
-        <ul>
-          <li><a href="openAcct.html">OPEN ACCOUNT</a></li>
-          <li><a href="everyBank.html">EVERYDAY BANKING</a></li>
-          <li><a href="loan.html">LOANS</a></li>
-          <li><a href="about.html">ABOUT</a></li>
-          <li><a href="contact.html">CONTACT</a></li>
-        </ul>
-        <div class="divlogin-btn">
-          <input type="button" class="login-btn" name="" value="LOGIN">
-        </div>
-      </div>
-    </div>
-    
-    <!-- First Section -->
-   
-        
-    </div>
-    <div class="btnonabtcontainer">
-      <a href="/everyBank.html" class="btn">OUR SERVICES</a>
-    </div>
-    <div class="btnonabtoa">
-      <a href="/openAcct.html" class="btnoa">OPEN ACCOUNT</a>
-    </div>
 
 
+<div class="loan-top">
+        <img src="<?php echo base_url('img/fist.png');?>" alt="" class="" >
+        <h2><span>Quick loans from OAMK Bank</span></h2>
+</div>
+<div class="loan-text">
+    <h2>Simple Unsecured Loans</h2>
+    <p>
+      We provide our customers with effortless access to loans without the need for collateral. <br>
+      Flexible terms for virtually any budget <br></p>
 
-    
-    <button onclick="topFunction()" id="Bttop" title="Go to top">Top</button>
-    <!-- Footer Section -->
-      <div class="footer" style="background-color :#FF9400">
-          <div class="footcontain">
-            <div>
-              <div class="copy"><small> Copyright &copy; 2018 Oamk Bank Oyj</small></div>
-              <div class="oamkfooter" style="color:#ffffff">Oamk Bank Oyj</div>
-              <div class="foot-text" style="color : #ffffff"> simple, fair and fast</div>
-            </div>
-            <div class="contactfoot">
-              <h3>Contact information</h3>
-              <p>Kotkantie 1<br/>
-              90250 Oulu<br/>
-            FINLAND<br/>
-  
-            <br/>
-            Tel: 040 1000 0000<br/>
-            email: bank@oamkbank.gq
-          </p><br/>
-        <!-- SoMe Icon Section -->    
-            </div>
-            <div class="somefooter">
-              <a href="https://www.facebook.com/oamkbank"><img style="margin-right: 20px;" src="img/fb.png" alt="" target="_blank">
-              <a href="https://plus.google.com/+oamkbank"><img style="margin-right: 20px;" src="img/gplus.png" alt="" target="_blank">
-              <a href="http://www.twitter.com/oamkbank"><img style="margin-right: 20px;" src="img/tw.png" alt="" target="_blank">
-              <a href="http://www.instagram.com/oamkbank"><img style="margin-right: 20px;" src="img/ig.png" alt="" target="_blank">
-            </div> 
-            <br/>
-          </div>
-          </div>
-      </div>
-      
-    </div>
+    <p>
+        <br>
+        <b>Online account management.</b> Check your loan and balance anytime, 
+        anywhere. <br>
+        <br>
+        <b>Easy payback options.</b>  Make quick one-time payments from any of your OAMK Bank account<br>
+        <br>
+    </p>
+</div>
+
+<div class="btnonloanoa">
+  <a href="<?php echo site_url('Page_Controller/openAcct');?>" class="btnoa">OPEN ACCOUNT</a>
+</div>
+ 
+<button onclick="topFunction()" id="Bttop" title="Go to top">Top</button>
